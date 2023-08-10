5858572f81242e3fc40f10530d2dfe9507701f27
5469810 <oukinme@gmail.com>
2023-08-10 15:14:18 +0900

    Update MonsterWebParser.cs

--- webScrapers/cvWebScraper/Services/MonsterWebParser.cs
+++ webScrapers/cvWebScraper/Services/MonsterWebParser.cs
@@ -1,14 +1,13 @@
-﻿using parser.Interfaces;
-using parser.Models;
+﻿using cvWebScraper.Interfaces;
+using cvWebScraper.Models;
 using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
-namespace parser.Services
+namespace cvWebScraper.Services
 {
     public class MonsterWebParser: IWebParser
     {
