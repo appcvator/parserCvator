53896fc778cb6245050896aa7af7062dbddf088d
5469810 <oukinme@gmail.com>
2023-08-10 12:39:42 +0900

    Update WebParser.cs

--- webScrapers/cvWebScraper/Services/WebParser.cs
+++ webScrapers/cvWebScraper/Services/WebParser.cs
@@ -15,7 +15,17 @@ namespace cvWebScraper.Services
             //can do reflection later. for now use Dictionary for fast implementation of IWebParser
             _parsers = new Dictionary<string, IWebParser>
             {
-                { "indeed.com", new IndeedWebParser() }
+                { "indeed.com", new IndeedWebParser() },
+                {"glassdoor.com", new GlassdoorWebParser() },
+                {"linkedin.com", new LinkedInWebParser() },
+                {"monster.com", new MonsterWebParser() },
+                {"ziprecruiter.com", new ZipRecruiterWebParser() },
+                {"chicago.craigslist.org", new ChicagoCraigslistWebParser() },
+                {"usajobs.gov", new UsaJobsWebParser() },
+                {"careerbuilder.co.uk", new CareerBuilderWebParser() },
+                {"simplyhired.com", new SimplyHiredWebParser() },
+                {"naukri.com", new NaukriWebParser() },
+                {"foundit.in", new FoundItInWebParser() }
             };
         }
 
